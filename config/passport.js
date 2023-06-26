60f59fc238a2ca30098e6026715d165b7af82721
Alex <kale51882cheryu@gmail.com>
2023-06-26 22:47:10 +0800

    change Jwtoptions

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 import jwtStrategy from 'passport-local';
 import extractJwt from 'passport-jwt';
-import User from '../models/userSchema.js'
+import User from '../models/userSchema.js';
 import dotenv from 'dotenv';
 dotenv.config()
 
@@ -12,7 +12,7 @@ const mypassport = (passport) => {
     
     Jwtoptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
     Jwtoptions.secretOrKey = process.env.secretOrKey;
-    passport.use(new JwtStrategy(jwtoptions, (jwt_payload, done) => {
+    passport.use(new JwtStrategy(Jwtoptions, (jwt_payload, done) => {
         User.findOne({ _id: jwt_payload._id }).then((user) => {
             if (user) {
                 done(null, user);
